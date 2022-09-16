2eb97c2e393a9ea16fe2e0ea6faf9127fa3f785f
Jo <113581172+Dyeoan@users.noreply.github.com>
2022-09-16 10:55:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 #Display fruityvice API response
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #writes data to the screen
 #streamlit.text(fruityvice_response.json()) 
 
